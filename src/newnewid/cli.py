3de99b9f4b6f1e9412f0129dc9ab88c4ddb82a50
quwabara <53551867+quwac@users.noreply.github.com>
2023-04-16 01:50:59 +0000

    Modify arguments

--- src/newnewid/cli.py
+++ src/newnewid/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import sys
 from typing import List
 
 from newnewid import (
@@ -25,80 +26,22 @@ from newnewid.parser.uuid_parser import parse
 
 
 def _get_uuid7_option(method: str) -> UUID7Option:
-    if method == "METHOD0":
+    if method == "METHOD-0":
         return METHOD_0_NO_COUNTER
-    elif method == "METHOD1-12":
+    elif method == "METHOD-1-12":
         return METHOD_1_FIXED_LENGTH_DEDICATED_COUNTER_BITS_12
-    elif method == "METHOD1-26":
+    elif method == "METHOD-1-26":
         return METHOD_1_FIXED_LENGTH_DEDICATED_COUNTER_BITS_26
     elif method == "METHOD-42":
         return METHOD_1_FIXED_LENGTH_DEDICATED_COUNTER_BITS_42
-    elif method == "METHOD2":
+    elif method == "METHOD-2":
         return METHOD_2_MONOTONIC_RANDOM_62_BITS
     else:
         raise ValueError(f"Invalid method: {method}")
 
 
-def generate_uuid():
+def generate_uuid(args):
     """Generate UUID."""
-    parser = argparse.ArgumentParser(description="Generate UUID")
-    parser.add_argument(
-        "n", type=int, nargs="?", help="Number of UUIDs to generate. example: 10", default=1
-    )
-    parser.add_argument(
-        "-u",
-        "--uuid-version",
-        type=str,
-        nargs="?",
-        help="Version of UUID. 1, 3, 4, 5, 6, 7, 8, nil or max",
-        default="7",
-    )
-    parser.add_argument(
-        "--namespace",
-        type=str,
-        nargs="?",
-        help="Namespace for UUIDv3 or UUIDv5. example: '0186b748-053d-7c24-be59-de03734bb9ab'",
-        default=None,
-    )
-    parser.add_argument(
-        "--name",
-        type=str,
-        nargs="?",
-        help="Name for UUIDv3 or UUIDv5. example: 'name'",
-        default=None,
-    )
-    parser.add_argument(
-        "--method",
-        nargs="?",
-        type=str,
-        help="Method for UUIDv7. 'METHOD0', 'METHOD1-12', 'METHOD1-26', 'METHOD1-42' or 'METHOD2'. Default: 'METHOD1-12'",
-        default="METHOD1-12",
-    )
-    parser.add_argument(
-        "--custom-a",
-        type=int,
-        nargs="?",
-        help="cuatom-a binary for UUIDv8.",
-        default=None,
-    )
-    parser.add_argument(
-        "--custom-b",
-        type=int,
-        nargs="?",
-        help="cuatom-b binary for UUIDv8.",
-        default=None,
-    )
-    parser.add_argument(
-        "--custom-c",
-        type=int,
-        nargs="?",
-        help="cuatom-c binary for UUIDv8.",
-        default=None,
-    )
-    args = parser.parse_args()
-
-    # --------------------------------------------------------------------------------
-
     uuid_version = args.uuid_version
 
     if uuid_version == "1":
@@ -151,28 +94,15 @@ def generate_uuid():
     elif uuid_version == "max":
         uuid_module = max_uuid
     else:
-        raise ValueError("Invalid version")
+        print("Invalid version.")
+        sys.exit(1)
 
     for _ in range(args.n):
         print(uuid_module())
 
 
-def parse_uuid():
+def parse_uuid(args):
     """Parse UUID."""
-    parser = argparse.ArgumentParser(description="Parse UUID")
-    parser.add_argument("uuid", nargs="+", type=str, help="UUID")
-    parser.add_argument(
-        "--method",
-        nargs="?",
-        type=str,
-        help="Method for UUIDv7. 'METHOD0', 'METHOD1-12', 'METHOD1-26', 'METHOD1-42' or 'METHOD2'. Default: 'METHOD1-12'",
-        default="METHOD1-12",
-    )
-
-    args = parser.parse_args()
-
-    # --------------------------------------------------------------------------------
-
     uuid_strs: List[str] = args.uuid
 
     for uuid_str in uuid_strs:
@@ -185,3 +115,86 @@ def parse_uuid():
             parsed = parse(uuid)
         obj = {**{"uuid": uuid_str}, **parsed}
         print(json.dumps(obj))
+
+
+def cli():
+    """CLI."""
+    parser = argparse.ArgumentParser(description="NewNewID")
+
+    subparsers = parser.add_subparsers(required=True)
+    gen_parser = subparsers.add_parser("generate", help="Generate UUID.")
+    gen_parser.set_defaults(func=generate_uuid)
+    gen_parser.add_argument(
+        "n",
+        type=int,
+        nargs="?",
+        help="Number of UUIDs to generate. example: 10",
+        default=1,
+    )
+    gen_parser.add_argument(
+        "-u",
+        "--uuid-version",
+        type=str,
+        help="Version of UUID. 1, 3, 4, 5, 6, 7, 8, nil or max",
+        required=True,
+    )
+    gen_parser.add_argument(
+        "--namespace",
+        type=str,
+        nargs="?",
+        help="Namespace for UUIDv3 or UUIDv5. example: '0186b748-053d-7c24-be59-de03734bb9ab'",
+        default=None,
+    )
+    gen_parser.add_argument(
+        "--name",
+        type=str,
+        nargs="?",
+        help="Name for UUIDv3 or UUIDv5. example: 'name'",
+        default=None,
+    )
+    gen_parser.add_argument(
+        "-m",
+        "--method",
+        nargs="?",
+        type=str,
+        help="Method for UUIDv7. 'METHOD-0', 'METHOD-1-12', 'METHOD-1-26', 'METHOD-1-42' or 'METHOD-2'. Default: 'METHOD-1-12'",
+        default="METHOD-1-12",
+    )
+    gen_parser.add_argument(
+        "--custom-a",
+        type=int,
+        nargs="?",
+        help="cuatom-a binary for UUIDv8.",
+        default=None,
+    )
+    gen_parser.add_argument(
+        "--custom-b",
+        type=int,
+        nargs="?",
+        help="cuatom-b binary for UUIDv8.",
+        default=None,
+    )
+    gen_parser.add_argument(
+        "--custom-c",
+        type=int,
+        nargs="?",
+        help="cuatom-c binary for UUIDv8.",
+        default=None,
+    )
+
+    parse_parser = subparsers.add_parser("parse", help="Parse UUID.")
+    parse_parser.set_defaults(func=parse_uuid)
+    parse_parser.add_argument("uuid", nargs="+", type=str, help="UUID")
+    parse_parser.add_argument(
+        "--method",
+        nargs="?",
+        type=str,
+        help="Method for UUIDv7. 'METHOD-0', 'METHOD-1-12', 'METHOD-1-26', 'METHOD-1-42' or 'METHOD-2'. Default: 'METHOD-1-12'",
+        default="METHOD-1-12",
+    )
+
+    try:
+        args = parser.parse_args()
+        args.func(args)
+    except Exception:
+        parser.print_help()
